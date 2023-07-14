4345fd277f31a93d676a7fd8002d4bf3e3cc86d0
Francksean <107214139+Francksean@users.noreply.github.com>
2023-07-14 19:32:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let firstName = document.getElementById('FirstName');
+let lastName = document.getElementById('LasrtName');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+let space = document.querySelectorAll('.remplissage');
+console.log(space);
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit', ()=>{
+  
+})
